add0fec2330a41dc46e9904e1326c77d9e379f3b
Arslan Ahmad <118178081+Arslan2214@users.noreply.github.com>
2023-07-13 21:20:59 +0500

    Update prectice.py

--- prectice.py
+++ prectice.py
@@ -93,3 +93,15 @@ if (op == '+')
 {
     print(f"{num1} + {num2}: {num1 + num2}")
 }
+if (op == '+')
+{
+    print(f"{num1} + {num2}: {num1 + num2}")
+}
+if (op == '+')
+{
+    print(f"{num1} + {num2}: {num1 + num2}")
+}
+if (op == '+')
+{
+    print(f"{num1} + {num2}: {num1 + num2}")
+}
